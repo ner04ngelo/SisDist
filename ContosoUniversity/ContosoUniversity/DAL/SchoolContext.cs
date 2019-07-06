5d50633edccd8027e69e2a165fd81e2c661a8240
NeroAngelo <jassonporras@hotmail.com>
2019-07-06 15:52:40 -0600

    added migrations and model more complex

--- ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -1,28 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using ContosoUniversity.Models;
 using System.Data.Entity;
-using System.Web;
-using ContosoUniversity.Models;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace ContosoUniversity.DAL
 {
-    public class SchoolContext: DbContext
+    public class SchoolContext : DbContext
     {
-
-        public SchoolContext() : base("SchoolContext")
-        {
-
-        }
-
-        public DbSet<Student> Estudiantes { get; set; }
-        public DbSet<Enrollment> Inscripciones { get; set; }
         public DbSet<Course> Cursos { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Inscripciones { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Estudiantes { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+                .Map(t => t.MapLeftKey("CourseID")
+                    .MapRightKey("InstructorID")
+                    .ToTable("CourseInstructor"));
         }
     }
 }
---
+++ ContosoUniversity/ContosoUniversity/Migrations/201907062130549_InitialCreate.cs
@@ -0,0 +1,22 @@
+namespace ContosoUniversity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Student", name: "FirstName", newName: "FirstMidName");
+            AlterColumn("dbo.Student", "LastName", c => c.String());
+            AlterColumn("dbo.Student", "FirstMidName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Student", "FirstMidName", c => c.String(maxLength: 50));
+            AlterColumn("dbo.Student", "LastName", c => c.String(maxLength: 50));
+            RenameColumn(table: "dbo.Student", name: "FirstMidName", newName: "FirstName");
+        }
+    }
+}
